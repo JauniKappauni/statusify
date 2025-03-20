6f68fdb68d39d242b64c733284d8495c3e096f3a
JauniKappauni <social.jauni@gmail.com>
2025-03-20 16:31:47 +0100

    Added function to check multiple hosts for online status and update status every 1000ms

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require('express');
+const ping = require('ping');
 const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
 
 const services = [
-    { name: "pve-01 IPv4", status: "Online ✅" },
-    { name: "pve-01 IPv6", status: "Online ✅" },
-    { name: "pve-02 IPv6", status: "Offline ❌" }
+    { name: "pve-01 IPv4", host: "1.1.1.1"},
+    { name: "pve-01 IPv6", host: "2606:4700:4700::1111"},
+    { name: "pve-02 IPv6", host: "2606:4700:4700::1001"}
 ];
+
+function checkStatus() {
+    services.forEach((services) => {
+        ping.sys.probe(services.host, (isOnline) => {
+            services.status = isOnline ? "Online ✅" : "Offline ❌"
+        })
+    })
+}
+
+setInterval(checkStatus, 1000);
 
 app.get('/', (req, res) => {
     res.render('index', { services });
